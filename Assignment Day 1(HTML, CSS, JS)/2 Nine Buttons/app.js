2a0b50b43f07c7d85d7b505df28db79ed206e15f
shadsheikh <shadsheikh3107@gmail.com>
2023-01-18 00:42:01 +0530

    task 2 completed

--- Assignment Day 1(HTML, CSS, JS)/2 Nine Buttons/app.js
+++ Assignment Day 1(HTML, CSS, JS)/2 Nine Buttons/app.js
@@ -1,11 +1,11 @@
-function myFun(){
-    var a=1;document.getElementById("1").innerHTML = a;
-    var b=2;document.getElementById("2").innerHTML = b;
-    var c=3;document.getElementById("3").innerHTML = c;
-    var d=4;document.getElementById("4").innerHTML = d;
-    var e=5;document.getElementById("5").innerHTML = e;
-    var f=6;document.getElementById("6").innerHTML = f;
-    var g=7;document.getElementById("7").innerHTML = g;
-    var h=8;document.getElementById("8").innerHTML = h;
-    var i=9;document.getElementById("9").innerHTML = i;
-}
+function rotate() {  
+    var viarableNum=btn1.innerHTML; 
+    btn1.innerHTML=btn4.innerHTML; 
+    btn4.innerHTML=btn7.innerHTML; 
+    btn7.innerHTML=btn8.innerHTML; 
+    btn8.innerHTML=btn9.innerHTML; 
+    btn9.innerHTML=btn6.innerHTML; 
+    btn6.innerHTML=btn3.innerHTML; 
+    btn3.innerHTML=btn2.innerHTML; 
+    btn2.innerHTML=viarableNum;  
+};
