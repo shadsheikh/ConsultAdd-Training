9d4520a95010e8b43ea60fbcc71e268c59396a9f
Shad Sheikh <shadsheikh3107@gmail.com>
2023-02-08 13:06:22 +0530

    Collections

--- Java/Collections.java
+++ Java/Collections.java
@@ -1,5 +1,13 @@
-package Java;
+import java.util.*;
 
 public class Collections {
-    
+    public static void main(String args[])
+    {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Shad", 10);
+        map.put("Sachin", 30);
+        map.put("Vaibhav", 20);
+        for (Map.Entry<String, Integer> e : map.entrySet())
+            System.out.println(e.getKey() + " " + e.getValue());
+    }
 }
