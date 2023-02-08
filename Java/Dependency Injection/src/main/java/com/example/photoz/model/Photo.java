8667256ffce214b3a5972d06cdc6d9533e497ce2
Shad Sheikh <shadsheikh3107@gmail.com>
2023-02-08 12:55:20 +0530

    Dependency Injection Done

---
+++ Java/Dependency Injection/src/main/java/com/example/photoz/model/Photo.java
@@ -0,0 +1,55 @@
+package com.example.photoz.model;
+
+import jakarta.validation.constraints.NotEmpty;
+import net.minidev.json.annotate.JsonIgnore;
+
+public class Photo {
+    private String id;
+    @NotEmpty
+    private String fileName;
+
+    private String contentType;
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    @JsonIgnore
+    private byte[] data;
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public Photo() {
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
