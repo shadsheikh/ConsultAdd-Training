5a4f13140143f1d7c59b722dc302d8948e11a43c
Shad Sheikh <shadsheikh3107@gmail.com>
2023-02-22 15:28:43 +0530

    Task 3 Completed

--- Express/routes/route.js
+++ Express/routes/route.js
@@ -2,6 +2,53 @@ const express = require("express")
 const userModel = require("../models/models")
 const router = express.Router();
 
+const createSchema = {
+    id: {
+        notEmpty: true,
+        errorMessage: "ID cannot be empty"
+    },
+    name: {
+        custom: {
+            options: value => {
+                return User.find({
+                    username: value
+                }).then(user => {
+                    if (user.length > 0) {
+                        return Promise.reject('Name already in use')
+                    }
+                })
+            }
+        }
+    },
+    email: {
+        normalizeEmail: true,
+        custom: {
+            options: value => {
+                return User.find({
+                    email: value
+                }).then(user => {
+                    if (user.length > 0) {
+                        return Promise.reject('Email address already taken')
+                    }
+                })
+            }
+        }
+    },
+    pwd: {
+        isStrongPassword: {
+            minLength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1
+        },
+        errorMessage: "Password must be greater than 8 and contain at least one uppercase letter, one lowercase letter, and one number",
+    },
+    role: {
+        notEmpty: true,
+        errorMessage: "Role cannot be empty"
+    }
+}
+
 // READ
 router.get('/getUser', async (req, res) => {
     const users = await userModel.find({});
@@ -27,14 +74,14 @@ router.get('/getUserById/:id', async (req, res) => {
  });
 
 // CREATE
-router.post('/addUser', async (req, res) => {
+router.post('/addUser', checkSchema(createSchema), async (req, res) => {
     const user = new userModel(req.body);
     try {
       await user.save();
         res.send(user);
-    } catch (error) {
+    }catch (error) {
         res.status(500).send(error);
-    }
+    } 
 });
 
 // UPDATE
@@ -61,21 +108,21 @@ router.put('/updateUser/:id', async (req, res) => {
     }
 });
 
-// DELETE
-// router.delete('/deleteUser/:id', async (req, res) => {
-//     const users = new userModel(req.body);
-//     let found = users.find(function (item) {
-//         return item.id === parseInt(req.params.id);
-//     });
-//     if (found) {
-//             let targetIndex = users.indexOf(found);
-//             users.splice(targetIndex,1);
-//         users.save(function(){});
-//             res.send(`accounts with id ${found.id} has been deleted`)
-//     }
-//     else{
-//         res.sendStatus(404);
-//     }
-// });
+DELETE
+router.delete('/deleteUser/:id', async (req, res) => {
+    const users = new userModel(req.body);
+    let found = users.find(function (item) {
+        return item.id === parseInt(req.params.id);
+    });
+    if (found) {
+            let targetIndex = users.indexOf(found);
+            users.splice(targetIndex,1);
+        users.save(function(){});
+            res.send(`accounts with id ${found.id} has been deleted`)
+    }
+    else{
+        res.sendStatus(404);
+    }
+});
 
 module.exports = router;
