e970bfbe6f1db50a03aa1ebaa0914621cedf9dfe
Om Bhatnagar <omishb2001@gmail.com>
2023-02-22 16:58:55 +0530

    Added .env file and packages for jwt authentication.

--- Express/app.js
+++ Express/app.js
@@ -1,9 +1,11 @@
-const express = require("express")
-const routes = require('./routes/route')
+const express = require("express");
+const routes = require("./routes/route");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const app = express()
+const app = express();
 
+dotenv.config();
 app.use(express.json());
 mongoose.set("strictQuery", false);
 
@@ -13,21 +15,25 @@ const cluster = "cluster0.1kwfpq4";
 const dbname = "Database";
 
 var query =
-   // `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`
-    'mongodb+srv://shads:shads123@cluster0.1kwfpq4.mongodb.net/?retryWrites=true&w=majority'
-const db = (query);
+	// `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`
+	"mongodb+srv://shads:shads123@cluster0.1kwfpq4.mongodb.net/?retryWrites=true&w=majority";
+const db = query;
 mongoose.Promise = global.Promise;
-mongoose.connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function (error) {
-    if (error) {
-        console.log("Error!" + error);
-    }
+mongoose.connect(
+	db,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	function (error) {
+		if (error) {
+			console.log("Error!" + error);
+		}
+	},
+);
+
+app.use("/", routes);
+
+app.listen(3000, () => {
+	console.log("listeniing at port:3000");
 });
-
-app.use('/', routes)
-
-app.listen(3000, ()=>{
-    console.log("listeniing at port:3000")
-})
